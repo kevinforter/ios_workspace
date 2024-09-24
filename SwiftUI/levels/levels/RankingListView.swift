849e827d3ef68976ab9eeb2e1484de6ba2e015e3
kevinforter <49223606+kevinforter@users.noreply.github.com>
2024-09-24 23:55:38 +0200

    added Player-Views to List
    added NavigationStack

---
+++ SwiftUI/levels/levels/RankingListView.swift
@@ -0,0 +1,18 @@
+//
+//  RankingListView.swift
+//  levels
+//
+//  Created by Kevin Forter on 24.09.2024.
+//
+
+import SwiftUI
+
+struct RankingListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RankingListView()
+}

9481dfd979596ba1b5af1e104b34b4dd2f230fd8
kevinforter <49223606+kevinforter@users.noreply.github.com>
2024-09-25 01:19:24 +0200

    moved files

---
+++ Levels/Levels/LevelsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LevelsApp.swift
+//  Levels
+//
+//  Created by Kevin Forter on 25.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct LevelsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Levels/LevelsTests/LevelsTests.swift
@@ -0,0 +1,17 @@
+//
+//  LevelsTests.swift
+//  LevelsTests
+//
+//  Created by Kevin Forter on 25.09.2024.
+//
+
+import Testing
+@testable import Levels
+
+struct LevelsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

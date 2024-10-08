fd2aeb547ba50b5f0e6d98658655670900339b5a
kevinforter <49223606+kevinforter@users.noreply.github.com>
2024-10-08 21:55:46 +0200

    added Setting and Overview file

---
+++ Levels/Levels/Overview/LevelsOverview.swift
@@ -0,0 +1,18 @@
+//
+//  LevelsOverview.swift
+//  Levels
+//
+//  Created by Kevin Forter on 08.10.2024.
+//
+
+import SwiftUI
+
+struct LevelsOverview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LevelsOverview()
+}
---
+++ Levels/Levels/Settings/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  Levels
+//
+//  Created by Kevin Forter on 08.10.2024.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}

cbe9594311ae2ed1eecc46a16f68609f4abfc798
kevinforter <49223606+kevinforter@users.noreply.github.com>
2024-10-30 00:15:06 +0100

    added singleton and loading rankings

--- Levels/Levels/Views/RankingListView.swift
+++ Levels/Levels/Views/RankingListView.swift
@@ -8,14 +8,48 @@
 import SwiftUI
 
 struct RankingListView: View {
+    @State private var syncManager = SyncManager.shared
+    @State private var rankedUsers: [RankedUser] = []
+    
     var body: some View {
         NavigationStack {
-        List {
-            RankingPlayerView(firstName: "Kevin", codeName: "The Student", score: "1000")
-            RankingPlayerView(firstName: "Elia", codeName: "The Designer", score: "900")
-            RankingPlayerView(firstName: "Jovan", codeName: "The Salesman", score: "800")
+            List(rankedUsers) { rankedUser in
+                RankingPlayerView(
+                    firstName: rankedUser.user.name,
+                    codeName: "The \(rankedUser.user.nickname)",
+                    score: "\(rankedUser.successCount)",
+                    profileImageUrl: rankedUser.user.imageUrl
+                )
+            }
+            .navigationBarTitle("Ranking List")
+            .task {
+                await loadRankings()
+            }
+            .refreshable {
+                await loadRankings()
+            }
         }
-        .navigationBarTitle("Ranking List")
+        
+        /*
+         NavigationStack {
+         List {
+         RankingPlayerView(firstName: "Kevin", codeName: "The Student", score: "1000")
+         RankingPlayerView(firstName: "Elia", codeName: "The Designer", score: "900")
+         RankingPlayerView(firstName: "Jovan", codeName: "The Salesman", score: "800")
+         }
+         .navigationBarTitle("Ranking List")
+         }
+         */
+    }
+    
+    private func loadRankings() async {
+        do {
+            try await syncManager.updateRanking()
+                        DispatchQueue.main.async {
+                            self.rankedUsers = syncManager.ranking
+                        }
+        } catch {
+            print("Failed to load rankings: \(error)")
         }
     }
 }
